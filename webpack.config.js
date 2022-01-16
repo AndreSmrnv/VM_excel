f1461154a4214cb45f0898b1a80d5d7078860185
AndreSmrnv <tehmir@bk.ru>
2022-01-16 14:16:55 +0300

    webpack - HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
-
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+//const CopyPlugin = require("copy-webpack-plugin")
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -11,5 +12,27 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
-    }
+    },
+    resolve: {
+        extensions: ['.js', '.ts', 'jsx', 'tsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src', 'core'),
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new FaviconsWebpackPlugin('./favicon_excel.png')
+        // new CopyPlugin({
+        //     patterns: [
+        //         {
+        //             from: path.resolve(__dirname, 'src', 'favicon.ico'),
+        //             to: path.resolve(__dirname, 'dist')
+        //         },
+        //     ],
+        // })
+
+    ]
 }
