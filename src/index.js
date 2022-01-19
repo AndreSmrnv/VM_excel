5c95f48a55f486f3b5e35cb2711873bee929d644
AndreSmrnv <tehmir@bk.ru>
2022-01-19 21:11:00 +0300

    add components to excel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import Excel from '@/components/Excel';
+import Excel    from '@/components/Excel';
+import Header   from '@/components/Header';
+import Toolbar  from '@/components/Toolbar';
+import Formula  from '@/components/Formula';
+import Table    from '@/components/Table';
 import './less/index.less';
 
 const excel = new Excel(
     '#app',
     {
-              components: []
-            }
+      components: [
+        Header,
+        Toolbar,
+        Formula,
+        Table
+      ]
+    }
 )
 
-console.log('Excel components', excel)
+excel.render()
 
