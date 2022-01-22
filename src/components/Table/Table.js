98b93b5a88d9cd545da24fac3ce9fcf0b4939721
AndreSmrnv <tehmir@bk.ru>
2022-01-22 19:05:13 +0300

    add dataset resizing

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,7 +4,20 @@ import {createTable} from '@/components/Table';
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown']
+        })
+    }
+
     toHTML() {
         return createTable()
     }
+
+    onMousedown(event) {
+        if (event.target.dataset.resize) {
+            console.log('Start resizing', event.target.dataset.resize)
+        }
+    }
+
 }
